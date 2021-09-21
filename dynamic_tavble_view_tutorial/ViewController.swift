e7eabb45d1371f9caa6f59d20089b03f04d4070e
kokojong <duwhd0349@kookmin.ac.kr>
2021-09-21 16:56:41 +0900

    Initial Commit

---
+++ dynamic_tavble_view_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dynamic_tavble_view_tutorial
+//
+//  Created by kokojong on 2021/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
